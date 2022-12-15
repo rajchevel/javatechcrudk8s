67baad86ca33cc417786d754c72b303bb470f8c1
rajchevel <49225731+rajchevel@users.noreply.github.com>
2022-12-15 19:12:14 +0530

    crud prog

---
+++ src/main/java/com/example/crud/JavaTechiCrudK8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechiCrudK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTechiCrudK8sApplication.class, args);
+	}
+
+}
